ccc5b89a626aa0f25f38b3a8e0a13f659bd13d9e
Leonardo Ginting <leonardo.ginting@allianz.co.id>
2020-04-02 14:46:21 +0700

    - Add Form for Input Jemaat later will add more function in the back end not yet developed, only additional validation for input

--- src/app/data/jemaat/form/input-jemaat/input-jemaat.component.ts
+++ src/app/data/jemaat/form/input-jemaat/input-jemaat.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-input-jemaat',
@@ -10,6 +11,25 @@ export class InputJemaatComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    function setFormValidation(id) {
+      $(id).validate({
+        highlight: element => {
+          $(element).closest('.form-group').removeClass('has-success').addClass('has-danger');
+          $(element).closest('.form-check').removeClass('has-success').addClass('has-danger');
+        },
+        success: element => {
+          $(element).closest('.form-group').removeClass('has-danger').addClass('has-success');
+          $(element).closest('.form-check').removeClass('has-danger').addClass('has-success');
+        },
+        errorPlacement: (error, element) => {
+          $(element).closest('.form-group').append(error);
+        },
+      });
+    }
+
+    $(document).ready(() => {
+      setFormValidation('#TypeValidation');
+    });
   }
 
 }
